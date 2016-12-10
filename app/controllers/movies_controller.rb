1fc278c880a52e81ecd92bc2ed9941d111ffb3b4
Anastasia <b.anastasya@bk.ru>
2016-12-10 15:15:45 -0500

    lalala

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,12 @@
+class MoviesController < ApplicationController
+  def search
+    @genres = TheMovieDbService.genres
+    # @movie = TheMovieDbService.new(params).search.sample
+    # Movie.create(title: @movie['title'])
+    # @movies = HTTParty.get("https://api.themoviedb.org/3/discover/movie?with_genres=28&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=4722ab521bc96d24058c095db629b2cd")
+  end
+
+  def search_result
+    @movie = TheMovieDbService.new(params).search.sample
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'movies/search'
+  get 'movies/result'
+  get 'movies/search_result'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
