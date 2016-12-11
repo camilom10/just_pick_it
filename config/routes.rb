3ed632ce9820a32e5c72376b8a9b4ccc295e144f
Anastasia <b.anastasya@bk.ru>
2016-12-11 17:59:29 -0500

    kfkfkf

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+    resources :users
   get 'movies/search'
-  get 'movies/result'
   get 'movies/search_result'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
-  root 'users#new'
+
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
